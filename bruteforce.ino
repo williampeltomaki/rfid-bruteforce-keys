744a32ec751d9ec486bac900d8451366513b26f2
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2020-09-05 20:09:49 -0300

    fixing up somethings

--- bruteforce.ino
+++ bruteforce.ino
@@ -45,51 +45,56 @@ void dump_byte_array(byte *buffer, byte bufferSize) {
 }
 
 bool try_key(byte block, MFRC522::MIFARE_Key *key) {
-    bool result = false;
     byte buffer[18];
-
     MFRC522::StatusCode status;
+
     status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, key, &(mfrc522.uid));
-    if (status != MFRC522::STATUS_OK) { return result; }
+    if (status != MFRC522::STATUS_OK) return false;
 
     byte byteCount = sizeof(buffer);
     status = mfrc522.MIFARE_Read(block, buffer, &byteCount);
-    if (status != MFRC522::STATUS_OK) { return result; } 
-    else {
-        result = true;
+
+    if (status == MFRC522::STATUS_OK) {
         Serial.print(F("Success with key:"));
         dump_byte_array((*key).keyByte, MFRC522::MF_KEY_SIZE);
         Serial.println();
 
         Serial.print(F("Block ")); Serial.print(block); Serial.print(F(":"));
         dump_byte_array(buffer, 16);
+
         Serial.println();
+        Serial.println();
+
+        return true;
     }
 
     Serial.println();
-    return result;
+    return false;
 }
 
 void loop() {
-    if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial()) return;
+    if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) return;
 
     Serial.print(F("Card UID:"));
     dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
     Serial.println();
 
     MFRC522::MIFARE_Key key;
-    for(byte block = 0; block < 64; block++ ){
+    for(byte block = 0; block < 64; block++){
         for(byte k = 0; k < NR_KNOWN_KEYS; k++){
             for (byte i = 0; i < MFRC522::MF_KEY_SIZE; i++) {
                 key.keyByte[i] = knownKeys[k][i];
             }
 
             if(try_key(block, &key)) break;
-            if (! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial()) break;
+            if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) break;
         }
 
         if(block == 63) {
-            Serial.println("Last block tried...");
+            Serial.println("Finishing...");
         }
     }
+
+    mfrc522.PICC_HaltA();
+    mfrc522.PCD_StopCrypto1();
 }
